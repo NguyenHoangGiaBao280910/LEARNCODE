0dfc06ee1db7ae29b9ae0706ee6b21e6edec2920
Nguyễn Hoàng Gia Bảo <nguyenhoanggiabaocongviec@gmail.com>
2025-12-05 18:07:31 +0700

    first commit

---
+++ Code.cpp
@@ -0,0 +1,24 @@
+// Số nguyên
+// int = 4 byte = 32 bit
+// longlong = 8 byte = 64bit
+// unsinged int ( khong dau )
+// unsinged longlong ( khong dau )
+// So thuc
+// float = 4 byte = 32 bit ( thap phan )
+// double = 8 byte = 64 bit ( thap phan )
+// Ki tu = char = 1 byte = 8 bit ( nhap chu )
+// Dung sai = bool = true,false = 1 byte ( -> ra 1 va o vi trong c++ quy dinh true false la 1 0 )
+// endl = xuong dong khi ket thuc 1 doan ( su dung neu muon xuong dong con ko thi ko sai )
+// "" khi sai trong cout no co nghia la in ra terminal
+#include <iostream>
+using namespace std;
+#define PI 3.14 
+int main()
+{
+  int a , b ; 
+  cout << " me m beo " ; 
+  return 0 ; 
+}
+// Bien = <kieu du lieu>  <ten bien>  = <gia tri>;
+// double chuvi = 3.14;
+// fixed << setprecision(n)( # include <ionasip> )  (lam tron so n )
---
+++ code2.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+#include < conio.h> 
+int main (){
+    int i =1 , n 
+    long S = 0 
+   printf ("\nNhap n : "); 
+   scanf("%d" , &n); 
+   while (i <= n ) ; 
+   {
+    S= S + i; 
+    i++ ; 
+ 
+   }
+
+   printf ("\nTong 1+2 + ... + %d la %ld " : n , S  ) ; 
+   getch () ; 
+   return 0 ; 
+
+
+}
